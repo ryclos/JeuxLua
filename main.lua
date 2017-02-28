defcc5e5e727346b1755309b16dbded96b6c2a20
ryclos <loogle@hotmail.com>
2017-02-27 22:57:53 -1000

    #Test 6 Adaptation & Compréhension
    Début de compréhension du langage Lua avec Love2D

--- main.lua
+++ main.lua
@@ -36,26 +36,20 @@ print("Energie du boss", boss.energy)
 
 
 -- Les fonction pour faire apparaitre la fenêtre Love/img/son --
---function love.load()
-   --love.mouse.setVisible(false) -- make default mouse invisible
-   --img = love.graphics.newImage("img/mouse.png") -- load in a custom mouse image
---end
---function love.draw()
-   --local x, y = love.mouse.getPosition() -- get the position of the mouse
-   --love.graphics.draw(img, x, y) -- draw the custom mouse image
---end
-function love.draw()
-  love.graphics.print("Hello World!", 400, 300)
-end
-function love.load()
-  whale = love.graphics.newImage("img/whale.png")
-end
-function love.load() -- love.draw normalement mais ça ne passe pas
-  love.graphics.draw(whale, 200, 100)
-end
+
 function love.load()
   sound = love.audio.newSource("music/access.ogg")
   love.audio.play(sound)
+  whale = love.graphics.newImage("img/whale.png")
+  love.mouse.setVisible(false) -- make default mouse invisible
+  img = love.graphics.newImage("img/mouse.png") -- load in a custom mouse image
+end
+--------------------------------------
+function love.draw()
+  love.graphics.print("Hello World!", 400, 300)
+  love.graphics.draw(whale, 200, 100)
+  local x, y = love.mouse.getPosition() -- get the position of the mouse
+  love.graphics.draw(img, x, y) -- draw the custom mouse image
 end
 --function love.resize(w, h) -- J'essaye de faire les rezize fenêtre lol
   --print(("Window resized to width: %d and height: %d."):format(1200, 900))
