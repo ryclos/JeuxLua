5d144447bdae44e7b9f85e7abf4ac9212e9faf69
ryclos <loogle@hotmail.com>
2017-02-27 19:46:35 -1000

    #Test 4
    Insertion et familiarisation du langage Lua et de ses fonctionnalité (
    Tel que els fonction par exemple xD)

--- main.lua
+++ main.lua
@@ -2,10 +2,15 @@ hero = {}
 hero.life = 0
 hero.energy = 0
 
-function startGame()
+boss = {}
+
+boss.energy = 0
+
+function startGame()-- Initialise a chaque fois (Test)
   print("La fonction a été appelée")
   hero.energy = 100
   hero.life = 5
+  boss.energy = 1000
 end
 
 print("nombre de vies:",hero.life)
@@ -16,9 +21,21 @@ print("nombre de vies:",hero.life)
 startGame()
 print("nombre de vies:",hero.life)
 
-print("Terminé")
+print("Appel de la fonction terminé")
+
+function Touche(personnage, nbPoints)-- Personnage prends les valeur heros et boss
+  personnage.energy = personnage.energy - nbPoints
+end
+
+print("nombre de vies:",hero.energy)
+Touche(hero, 5)
+print("nombre de vies:",hero.energy)
+
+Touche(boss, 45)
+print("Energie du boss", boss.energy)
 
 
+-- Les fonction pour faire apparaitre la fenêtre Love/img/son --
 function love.draw()
   love.graphics.print("Hello World!", 400, 300)
 end
